88da576f0ba3c84971d1f668c30211336d854dc2
maicond02 <eumaicondouglas.md45@gmail.com>
2023-01-24 16:31:02 -0300

    navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="container">
+      <ul>
+        <li v-for="page in pages" :key="pages.id">
+          <a :href="page.route">{{page.name}}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+              pages:[
+                { id: 1,name: 'HOME', route: '/'},
+                { id: 2,name: 'CALCULATOR', route: 'google.com'},
+                { id: 3,name: 'ABOUT' },
+              ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .container{
+    width: 100%;
+    height: 60px;
+    background-color: rgb(20, 36, 126);
+    display: flex;
+    align-items: center;
+  }
+  ul li{
+    list-style: none;
+    display: inline;
+  }
+  ul li a{
+    color: white;
+    font-style: normal;
+    text-decoration: none;
+    margin-left: 10px;
+  }
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~styleGlobal/style.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
